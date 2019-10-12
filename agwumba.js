51d70ea6269ead86a087cf67b5886e828ab7039c
CENWACHUKWU <CENWACHUKWUU@GMAIL.COM>
2019-10-11 20:24:21 -0400

    created-actual-deck

--- agwumba.js
+++ agwumba.js
@@ -13,5 +13,19 @@ class Deck {
         this.cards = [] //an array of cards in the deck
         // this.draw = draw //return a random card from the cards array
         this.getDeck()
-      }
+    }
+    getDeck() {
+        const cardValue = ['a', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
+        const suits = ['hearts', 'spades', 'diamonds', 'clubs'];
+        const ranking = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
+        for (let i = 0; i < suits.length; i++) {
+          for (let j = 0; j < cardValue.length; j++) {
+            const newCard = new Card(cardValue[j], suits[i], ranking[j])
+            this.cards.push(newCard);
+          }
+          // return ('newCard')
+        }
+    
+    }
+    // const gameDeck = new Deck()
 } 
