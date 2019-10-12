478dc8bd5b06684744e7cf3dbd7e7e68e0aca5f6
CENWACHUKWU <CENWACHUKWUU@GMAIL.COM>
2019-10-11 20:19:24 -0400

    created-deck-class

--- agwumba.js
+++ agwumba.js
@@ -5,3 +5,13 @@ class Card {
       this.ranking = ranking
     }
 }
+
+class Deck {
+    constructor() {
+        this.length = 52;
+        // this.length = //the number of cards - should start at 52
+        this.cards = [] //an array of cards in the deck
+        // this.draw = draw //return a random card from the cards array
+        this.getDeck()
+      }
+} 
